601f99330124f6be14e5a76c3cf4dd0c7531fa3b
adeliaayu <adeliaayut53@gmail.com>
2020-09-05 16:31:27 +0700

    add simple work manager with one time task

--- app/src/main/java/com/dicoding/kotlin/myworkmanager/MainActivity.kt
+++ app/src/main/java/com/dicoding/kotlin/myworkmanager/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.dicoding.kotlin.myworkmanager
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.work.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOneTimeTask.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btnOneTimeTask -> startOneTimeTask()
+        }
+    }
+
+    private fun startOneTimeTask() {
+        textStatus.text = getString(R.string.status)
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, editCity.text.toString())
+            .build()
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setInputData(data)
+            .setConstraints(constraints)
+            .build()
+        WorkManager.getInstance().enqueue(oneTimeWorkRequest)
+        WorkManager.getInstance().getWorkInfoByIdLiveData(oneTimeWorkRequest.id).observe(this@MainActivity, object : Observer<WorkInfo> {
+            override fun onChanged(workInfo: WorkInfo) {
+                val status = workInfo.state.name
+                textStatus.append("\n" + status)
+            }
+        })
     }
 }
